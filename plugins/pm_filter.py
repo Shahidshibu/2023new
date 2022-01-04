f6538bba7b889b961b8d9e15e055b0239c0d32d2
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2022-01-04 17:42:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2,6 +2,8 @@
 import asyncio
 import re
 import ast
+
+from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
