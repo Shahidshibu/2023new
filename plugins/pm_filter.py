040a07981d60e62b40fcc83a7ea2bfee1ac57269
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2022-01-04 09:39:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -71,7 +71,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("മോനെ (query.from_user.first_name) ഇത് നിനക്കുള്ളതല്ല 😉\n{message.from_first_name}ന്റെ റിക്യൂസ്റ്റ് ആണ് ഇത് 😜\nʀᴇǫᴜᴇᴇsᴛ ʏᴏᴜʀ ᴏᴡɴ 𝕙\n©️ ᴄɪɴᴇᴍᴀ ʟᴏᴋʜᴀᴍ ²ᐧ⁰", show_alert=True)
+        return await query.answer("മോനെ {query.from_user.first_name} ഇത് നിനക്കുള്ളതല്ല 😉\n{message.from_user.first_name}ന്റെ റിക്യൂസ്റ്റ് ആണ് ഇത് 😜\nʀᴇǫᴜᴇᴇsᴛ ʏᴏᴜʀ ᴏᴡɴ 𝕙\n©️ ᴄɪɴᴇᴍᴀ ʟᴏᴋʜᴀᴍ ²ᐧ⁰", show_alert=True)
     try:
         offset = int(offset)
     except:
