7056c3ff71351036de99e254ff0e2eb7302fe53e
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2022-01-04 17:44:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -32,9 +32,7 @@ STARTING_MSG = """
 
 @Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
 async def give_filter(client,message):
-    k = await manual_filters(client, message)
-    if k == False:
-        await auto_filter(client, message)   
+    k = await auto_filter(client, message)   
 
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
