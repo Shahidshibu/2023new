c039c0bb63754dc9af52f3c6ef0ca67cf7b861a1
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2022-01-04 14:51:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -78,7 +78,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(text=HEHE_TXT.format(query.from_user.first_name, update.from_user.first_name), show_alert=True)
+        return await query.answer(text=HEHE_TXT.format(query.from_user.first_name), show_alert=True)
     try:
         offset = int(offset)
     except:
