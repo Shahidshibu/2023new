f9e6d3ea47099a40fc24736b4abfe6699836595e
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2022-01-05 22:58:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -640,7 +640,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = cap.format(message.from_user.mention)
+        cap = NOR_TXT.format(message.from_user.mention)
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
