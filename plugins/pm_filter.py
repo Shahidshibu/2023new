01fe360fa87b63a3fe7b28e367c235453c1dab3e
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2022-01-23 14:47:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -640,24 +640,24 @@ async def auto_filter(client, msg, spoll=False):
                 InlineKeyboardButton(text="ᴍᴏᴠɪᴇ", callback_data='movieaada'),
                 InlineKeyboardButton(text="ꜱᴇʀɪᴇꜱ", callback_data='seriesaada')
             ],[
-                InlineKeyboardButton(text=f"◽ {get_size(file.file_size)} ▸ {file.file_name}", callback_data=f'{pre}#{file.file_id}'),
-            ]  
-            for file in files
-        ]
-    else:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"{file.file_name}",
-                    callback_data=f'{pre}#{file.file_id}',
-                ),
-                InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
-                    callback_data=f'{pre}_#{file.file_id}',
-                ),
+                InlineKeyboardButton(text=f"◽ {get_size(file.file_size)} ▸ {file.file_name}", callback_data=f'{pre}#{file.file_id}')
             ]
             for file in files
         ]
+        else:
+            btn = [
+                [
+                    InlineKeyboardButton(
+                        text=f"{file.file_name}",
+                        callback_data=f'{pre}#{file.file_id}',
+                    ),
+                    InlineKeyboardButton(
+                        text=f"{get_size(file.file_size)}",
+                        callback_data=f'{pre}_#{file.file_id}',
+                    ),
+                ]
+                for file in file
+            ]
 
     if offset != "":
         key = f"{message.chat.id}-{message.message_id}"
